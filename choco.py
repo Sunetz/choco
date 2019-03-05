5e60e2c212956ff76ae9dc1e01e97aab459b6be6
Sunetz <asuntcova@yandex.ru>
2019-03-05 20:36:34 +0300

    xxx

--- choco.py
+++ choco.py
@@ -9,10 +9,7 @@ def make_choco(small, big, goal):
         return goal - big*5
     else:
         return -1
-    if whole_part > big and goal <= small:
-        return goal
-    else:
-        return -1
+
 
 print(make_choco(6,1,10))
 print(make_choco(60,100,550))
